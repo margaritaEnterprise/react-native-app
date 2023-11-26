695a07337c98cb10a31d2c6c935772b4ae7e05eb
veroyols <vero.scholz@gmail.com>
2023-11-26 17:55:44 -0300

    get all countries

---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export type Location = {
+    lat: number; 
+    lng: number;
+};
+export type Country = {
+    cca2: string;
+    name: string;
+    flag: string; 
+    location: Location;
+    
+}
--- src/views/MasterScreen.tsx
+++ src/views/MasterScreen.tsx
@@ -1,14 +1,67 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, View, Text, TouchableOpacity, SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import getAll from '../services/fetch';
+import { Country, Location } from '../types/types';
+import ItemCountry from '../components/ItemCountry';
 
 function MasterScreen({navigation}) {
-  return (
-    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      <Text>Master Screen</Text>
-      <TouchableOpacity onPress={() => {navigation.navigate('Detail');}}>
-        <Text>Go to Detail</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    
+    const [allCountries, setAllCountries] = useState<Array<Country>>();
+   
+    useEffect(() => {
+        const loadCountries = async() => {
+            console.log("HOLA")
+            try {
+                const response = await getAll("cca2,translations,flags,latlng");
+                let array:Array<Country> = [];
+                response.map((item: any)=>{
+                    let l:Location = {
+                        lat: item.latlng[0],
+                        lng: item.latlng[1]
+                    }
+                    let c:Country = {
+                        cca2: item.cca2,
+                        name: item.translations.spa.common,
+                        flag: item.flags.png,
+                        location: {
+                            lat: item.latlng[0],
+                            lng: item.latlng[1]
+                        }
+                    };
+                    array.push(c);
+                });
+                setAllCountries(array);
+                console.log(array);
+            } catch(error) {
+                console.log(error)
+            }
+        } 
+        loadCountries();
+        
+    }, []);
+
+
+    return ( 
+        <View style={styles.container}>
+            <Text>Master Screen</Text>
+            <FlatList
+                data={allCountries}
+                renderItem={({item}) => <ItemCountry navigation={navigation} data={item} press={() => {navigation.navigate('Detail');}}/>}
+                keyExtractor={item => item.cca2}
+            />
+        </View>
+    
+    );
 }
+/**name={item.name} cca2={item.cca2} flag={item.flag} location={item.location}  */
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0, 
+        justifyContent:'center', 
+        alignItems:'center'
+    }
+});
+
 export default MasterScreen;
