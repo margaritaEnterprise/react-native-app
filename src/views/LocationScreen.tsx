12697e51761e6c6fec95f26c5c6dee86fc982880
NicoHerlan <nicodvkcss@gmail.com>
2023-11-26 15:05:06 -0300

    add mapview in location screen

--- src/views/LocationScreen.tsx
+++ src/views/LocationScreen.tsx
@@ -1,12 +1,102 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Platform } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
 
-function LocationScreen({navigation}) {
+function LocationScreen({ navigation }) {
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.015,
+    longitudeDelta: 0.0121,
+  });
+
+  useEffect(() => {
+    const getCurrentLocation = async () => {
+      try {
+        let permissionStatus;
+
+        if (Platform.OS === 'ios') {
+          permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+        } else {
+          permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+        }
+
+        if (permissionStatus !== RESULTS.GRANTED) {
+          const result = await requestLocationPermission();
+          if (result !== RESULTS.GRANTED) {
+            console.log('El usuario ha negado los permisos de ubicación.');
+            return;
+          }
+        }
+
+        Geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            setRegion({
+              latitude,
+              longitude,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            });
+          },
+          (error) => console.log(error),
+          { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        );
+      } catch (error) {
+        console.error('Error al obtener la ubicación:', error);
+      }
+    };
+
+    getCurrentLocation();
+
+    const locationInterval = setInterval(() => {
+      getCurrentLocation();
+    }, 5000);
+    return () => clearInterval(locationInterval);
+  }, []);
+
+  const requestLocationPermission = async () => {
+    try {
+      if (Platform.OS === 'ios') {
+        return await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      } else {
+        return await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      }
+    } catch (error) {
+      console.error('Error al solicitar permisos de ubicación:', error);
+    }
+  };
+
   return (
-    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      <Text>Location Screen</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={region}
+        zoomEnabled={true}
+        zoomControlEnabled={true}
+        showsMyLocationButton={true}
+        showsUserLocation={true}
+      >
+        <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 3,
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+  },
+});
 
 export default LocationScreen;
